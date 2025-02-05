01b99cbebaf026040807b9ec1b963e79e243b69d
lodjers <officialkovtun@gmail.com>
2025-02-05 12:42:47 +0300

    подобавляли сущности в БД через Hibernate

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,9 +1,6 @@
 package org.example.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Person")
@@ -11,6 +8,10 @@ public class Person {
 
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+    generator = "seq_generator_person")
+    @SequenceGenerator(name="seq_generator_person",
+    sequenceName = "person_id_seq", allocationSize = 1)
     private int id;
 
     @Column(name = "name")
@@ -21,8 +22,7 @@ public class Person {
 
     public Person() {}
 
-    public Person(int id, String name, int age) {
-        this.id = id;
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
